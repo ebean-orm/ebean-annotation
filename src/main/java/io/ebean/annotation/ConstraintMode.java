d0870fd39ac56848f1df86d37cb79bd6ffc56d19
Rob Bygrave <robin.bygrave@gmail.com>
2018-02-21 10:54:13 +1300

    #17 - ENH: Add @DbForeignKey and associated ConstraintMode
    Tidy up providing guidance to never change onUpdate and remove GLOBAL_DEFAULT and tell

--- src/main/java/io/ebean/annotation/ConstraintMode.java
+++ src/main/java/io/ebean/annotation/ConstraintMode.java
@@ -2,33 +2,34 @@ package io.ebean.annotation;
 
 /**
  * Modes for the DbForeignKey onDelete and onUpdate clause of a foreign keys.
+ * <p>
+ * Note that we should never change the <code>onUpdate</code> clause as primary keys
+ * should never change. Instead review the design ensuring the primary keys are immutable.
+ * Always leave the onUpdate mode at it's default of RESTRICT ('No Action').
+ * </p>
  *
  * @see DbForeignKey
  */
 public enum ConstraintMode {
 
 	/**
-	 * Mode defined as the global default which is usually RESTRICT.
+	 * Restrict - The usual default behavior for foreign constraints (also known as 'No Action').
 	 */
-	GLOBAL_DEFAULT,
+	RESTRICT,
 
 	/**
-	 * Set the value to null when a referenced id is deleted and or updated.
+	 * Set the value to null when a referenced id is deleted.
 	 */
 	SET_NULL,
 
 	/**
-	 * Set the value to 'default' when a referenced id is deleted and or updated.
+	 * Set the value to 'default' when a referenced id is deleted.
 	 */
 	SET_DEFAULT,
 
 	/**
-	 * Cascade - To automatically cascade a delete and or an update of a referenced id.
+	 * Cascade - To automatically cascade a delete of a referenced id.
 	 */
-	CASCADE,
+	CASCADE
 
-	/**
-	 * Restrict - The usual default behavior for foreign constraints.
-	 */
-	RESTRICT
 }
--- src/main/java/io/ebean/annotation/DbForeignKey.java
+++ src/main/java/io/ebean/annotation/DbForeignKey.java
@@ -31,10 +31,10 @@ import java.lang.annotation.Target;
  * }</pre>
  * <p>
  * <p>
- * <h3>Example: On delete set null and on update cascade</h3>
+ * <h3>Example: On delete set null</h3>
  * <pre>{@code
  *
- * @DbForeignKey(onDelete = ConstraintMode.SET_NULL, onUpdate = ConstraintMode.CASCADE)
+ * @DbForeignKey(onDelete = ConstraintMode.SET_NULL)
  * @ManyToOne
  * RelatedBean parent;
  *
@@ -47,12 +47,20 @@ public @interface DbForeignKey {
 	/**
 	 * Specify the onDelete mode. Typically will default to RESTRICT (aka No Action).
 	 */
-	ConstraintMode onDelete() default ConstraintMode.GLOBAL_DEFAULT;
+	ConstraintMode onDelete() default ConstraintMode.RESTRICT;
 
 	/**
-	 * Specify the onUpdate mode. Typically will default to RESTRICT (aka No Action).
+	 * Do NOT change this - seriously, don't do it.
+	 * <p>
+	 * Your primary keys should never change by design. We should orientate the design to
+	 * support primary keys that change (and instead find better non mutating primary keys).
+	 * Oracle go to the point of actively not supporting "on update" for this reason.
+	 * </p>
+	 * <p>
+	 * So yes, we can specify the onUpdate mode but I don't expect anyone to change this.
+	 * </p>
 	 */
-	ConstraintMode onUpdate() default ConstraintMode.GLOBAL_DEFAULT;
+	ConstraintMode onUpdate() default ConstraintMode.RESTRICT;
 
 	/**
 	 * Set to true when we do not wish any foreign key constraint to be created.
