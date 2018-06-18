54d2aa826dc2d3a9da7dbe43ffc9c7a977555544
rob bygrave <robin.bygrave@gmail.com>
2018-06-18 20:59:02 +1200

    #22 - Remove PersistBatch.INSERT ... migrate to PersistBatch.ALL

--- src/main/java/io/ebean/annotation/PersistBatch.java
+++ src/main/java/io/ebean/annotation/PersistBatch.java
@@ -13,7 +13,7 @@ package io.ebean.annotation;
  *
  *
  *   // set full jdbc batch per transaction
- *   transaction.setBatch(PersistBatch.ALL);
+ *   transaction.setBatchMode(true);
  *
  * }</pre>
  *
@@ -23,36 +23,16 @@ public enum PersistBatch {
   /**
    * Do not use JDBC Batch mode.
    */
-  NONE(false),
-
-  /**
-   * Use JDBC Batch mode on Inserts.
-   */
-  INSERT(true),
+  NONE,
 
   /**
    * Use JDBC Batch mode on Inserts, Updates and Deletes.
    */
-  ALL(true),
+  ALL,
 
   /**
    * You should not use this value explicitly. It should only used on the Transactional annotation
    * to indicate that the value should inherit from the ServerConfig setting.
    */
-  INHERIT(false);
-
-
-  final boolean forInsert;
-
-  PersistBatch(boolean forInsert) {
-    this.forInsert = forInsert;
-  }
-
-  /**
-   * Return true if persist cascade should use JDBC batch for inserts.
-   */
-  public boolean forInsert() {
-    return forInsert;
-  }
-
+  INHERIT
 }
