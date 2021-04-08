7ab4597ef53bccdf98f11bc531424ea2134d4641
Robin Bygrave <robin.bygrave@gmail.com>
2021-04-08 20:19:02 +1200

    #45 - ENH: Add support for experimental @Transactional autoPersistUpdates option

--- src/test/java/io/ebean/annotation/TxOptionTest.java
+++ src/test/java/io/ebean/annotation/TxOptionTest.java
@@ -13,4 +13,11 @@ public class TxOptionTest {
         assertThat(TxOption.ON.asBoolean()).isTrue();
         assertThat(TxOption.OFF.asBoolean()).isFalse();
     }
+
+    @Test
+    public void test_toString() {
+        assertThat(TxOption.DEFAULT.toString()).isEqualTo("DEFAULT");
+        assertThat(TxOption.ON.toString()).isEqualTo("ON");
+        assertThat(TxOption.OFF.toString()).isEqualTo("OFF");
+    }
 }
